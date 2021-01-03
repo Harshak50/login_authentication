8dbf62d3b2c08916ad7938b5f421fbc946511aca
Harsha <konndapuramharsha@gmail.com>
2021-01-03 16:37:59 +0530

    login

---
+++ main.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDi1yeRtySHX2PaDbWZ5VxrbSvVM5InzG8",
+    authDomain: "authentication-c53d6.firebaseapp.com",
+    projectId: "authentication-c53d6",
+    storageBucket: "authentication-c53d6.appspot.com",
+    messagingSenderId: "270441044653",
+    appId: "1:270441044653:web:97a9a5ec59aa7fe69b4424",
+    measurementId: "G-1EFG4LJ2D8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  
