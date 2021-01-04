fe6319e9b3fbd5ac60d98008bc724ad8d9b98241
Harsha <konndapuramharsha@gmail.com>
2021-01-04 22:20:44 +0530

    branch-1

--- main.js
+++ main.js
@@ -1,34 +1,46 @@
-            
+var config = {
+  apiKey: "AIzaSyDi1yeRtySHX2PaDbWZ5VxrbSvVM5InzG8",
+authDomain: "authentication-c53d6.firebaseapp.com",
+projectId: "authentication-c53d6",
+storageBucket: "authentication-c53d6.appspot.com",
+messagingSenderId: "270441044653",
+appId: "1:270441044653:web:97a9a5ec59aa7fe69b4424",
+measurementId: "G-1EFG4LJ2D8"
+};
+
+firebase.initializeApp(config);          
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       // User is signed in.
-      document.getElementById("login-div").style.display = "none";
-      document.getElementById("loggedin-div").style.display = "block";
-      document.getElementById("signup-div").style.display = "none";
-      
+    document.getElementById("login-div").style.display = "none";
+    document.getElementById("signup-div").style.display = "none";
+    window.location = "/Home.html";
+   
 
-    var user = firebase.auth().currentUser;
-
-    if(user != null){
-
-      var email_id = user.email;
-      document.getElementById("user").innerHTML = "Welcome  : " + email_id;
-
-    }
-
-    } else {
+   }
+    else {
       // No user is signed in.
       document.getElementById("login-div").style.display = "block";
-      document.getElementById("loggedin-div").style.display = "none";
       document.getElementById("signup-div").style.display = "none";
     }
+      
   });
+  firebase.auth().onAuthStateChanged(function(user) {
+  var user = firebase.auth().currentUser;
+  if(user != null){
+    var email_id = user.email;
+    document.getElementById("user").innerHTML = "Welcome  : " + email_id;
+  }
+ 
+});
+
+
+  document.getElementById("user").innerHTML = "Welcome  : " + email_id;
 
 
   //  function to open signup page
   function signuppage(){
     document.getElementById("login-div").style.display = "none";
-    document.getElementById("loggedin-div").style.display = "none";
     document.getElementById("signup-div").style.display = "block";
   }
 
@@ -66,7 +78,8 @@ var check = function() {
   function login(){
     var userEmail = document.getElementById("inputEmail").value;
     var userPassword = document.getElementById("inputPassword").value;
-
+    
+     
     firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
       // Handle Errors here.
       var errorCode = error.code;
@@ -95,7 +108,5 @@ var check = function() {
 
 function logout(){
   firebase.auth().signOut();
+  window.location = 'index.html';
 }
-
-  
-  
